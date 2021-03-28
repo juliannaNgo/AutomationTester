2cccafc38f7879727321d49274b57f3a873d3b9a
Julianna Ngo <49851756+juliannaNgo@users.noreply.github.com>
2021-03-28 18:39:08 -0400

    Update Tester.java
    Added Google Maps Launch flow.
    Added Google Apps Menu navigation flow using JS Executor.
    Added close browser function after execution.

--- src/Tester.java
+++ src/Tester.java
@@ -1,32 +1,30 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Tester implements Data{
+public class Tester extends Data{
 	
 	public static WebDriver driver = null;
 
 	public static void main(String[] args) {
 		//Setting Chromedriver to chromedriver.exe in project folder
 		System.setProperty("webdriver.chrome.driver", ".\\webdrivers\\chromedriver.exe");
-		driver = new ChromeDriver();
+		
+		//Create and setting ChromeDriver options to start browser in incognito-mode & maximized-window
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--incognito");
+		options.addArguments("start-maximized");
+		WebDriver driver = new ChromeDriver(options);
 		boolean result = true;
+		
 		//Creating WebDriverWait object for element visibility waiting, with TimeOut timer of 20 seconds
 		WebDriverWait wait = new WebDriverWait(driver, 20);
-		
-		ChromeOptions options = new ChromeOptions();
-		//Enable incognito in chrome options (avoid cache impact on RT)
-		options.addArguments("--incognito");
-		//Enable maximized window in start-up
-		options.addArguments("--start-maximized");
-		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-		capabilities.setCapability(ChromeOptions.CAPABILITY,  options);
 
 		try {
 			//Launch Google Chrome and open URL
@@ -36,10 +34,37 @@ public class Tester implements Data{
 			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(searchBar)));		
 			WebElement inputBox = driver.findElement(By.xpath(searchBar));
 			inputBox.sendKeys("Google" + "\n");
-			//Click News Button
 			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(searchResult)));	
-			WebElement newsButton = driver.findElement(By.xpath(newsBtn));
-			newsButton.click();
+			
+			//Click Maps Button
+			Data.clickAndWait(driver, wait, mapsBtn, runBtn);
+			
+			//Click App Menu
+			Data.clickAndWait(driver, wait, appMenu, menuFrame);
+			
+			//Switch window context to appMenu window
+			WebElement appFrame=driver.findElement(By.xpath(menuFrame));
+			driver.switchTo().frame(appFrame);
+			
+			//Scroll Down App Menu Until More Apps Button Visible
+			WebElement moreAppButton = driver.findElement(By.xpath(moreAppsBtn));
+			((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", moreAppButton);
+			
+			//Click More Applications Button
+			moreAppButton.click();
+			
+			//Switch window context to Products window
+			for (String windowHandler : driver.getWindowHandles()) {
+				driver.switchTo().window(windowHandler);
+			}
+			
+			//Scroll Down Page Until Sheets Button is Visible
+			WebElement sheetsButton = driver.findElement(By.xpath(sheetBtn));
+			((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", sheetsButton);
+			
+			//Click Sheets Button
+			sheetsButton.click();
+			
 		}
 		catch(TimeoutException e) {
 			result = false;
@@ -51,6 +76,9 @@ public class Tester implements Data{
 		}
 		
 		System.out.println("Test result: " + result);
+		
+		//Close all Browser windows
+		driver.quit();
 	}
 
 }
